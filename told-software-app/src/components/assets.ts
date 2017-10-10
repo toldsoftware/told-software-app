f59668b83147deb277f1261a3d1c4002971ad2e4
Rick Love <rick@toldpro.com>
2017-10-10 07:41:36 -0500

    FIX Expo (Use IDE)

--- told-software-app/src/components/assets.ts
+++ told-software-app/src/components/assets.ts
@@ -1,1 +1,2 @@
-export const image_rick = require('../../img/rick.jpg');
+export const image_rick = require('../../assets/img/rick.jpg');
+export const image_toldLogo128 = require('../../assets/img/told-logo-128.png');
